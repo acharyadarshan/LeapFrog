658a562e8e948f93ab89a3dbb9882b2ef2ca9acb
Darshan Acharya <acharyadarshan02@gmail.com>
2022-06-02 00:12:19 +0545

    component design commit

--- Assignment/Assignment_4/js/index.js
+++ Assignment/Assignment_4/js/index.js
@@ -1,31 +1,17 @@
-// console.log("hey");
-
-// const hamburger = document.getElementById("hamburger");
-// const navMenu = document.getElementsByClassName("activate");
-
-// hamburger.addEventListener("click", () => {
-//   //alert("hello");
-//   // navMenu.classList.toggle("show");
-//   navMenu.style.display = "show";
-// });
-
 var visible = 1;
 const menu = document.getElementById("menu");
 const content = document.getElementById("belowSection");
 
 function clicker() {
   if (visible % 2 == 1) {
-    menu.style.display = "block";
+    // menu.style.display = "block";
+    menu.classList.add("activate");
     content.style.display = "none";
+    menu.style.display = "block";
   } else {
-    menu.style.display = "none";
+    menu.style.display = "block";
+    menu.classList.remove("activate");
     content.style.display = "block";
   }
   visible += 1;
 }
-
-// function clicker() {
-//   // menu.style.display = "block";
-//   menu.classList.toggle("show");
-//   // content.style.display = "none";
-// }
